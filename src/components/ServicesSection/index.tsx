0d270fc8bad9aab5dea6a2146aa06477132eded9
IamCooller <45273146+IamCooller@users.noreply.github.com>
2024-02-28 13:30:16 -0700

    Update index.tsx

--- src/components/ServicesSection/index.tsx
+++ src/components/ServicesSection/index.tsx
@@ -25,6 +25,12 @@ export const ServicesSection = ({
   title: string;
   subtitle: string;
 }) => {
+  const isMobile =
+    typeof window !== 'undefined' ? window.innerWidth <= 768 : false;
+  const isTouch =
+    typeof window !== 'undefined'
+      ? window.matchMedia('(pointer: coarse)').matches
+      : false;
   return (
     <section className=" pb-20 lg:pb-44">
       <div className="container">
@@ -37,7 +43,7 @@ export const ServicesSection = ({
             opts={{
               align: 'start'
             }}
-            enableScrollSnap={true}
+            enableScrollSnap={isMobile || isTouch ? false : true}
             className="w-full"
           >
             <div className="relative max-md:-ml-6 max-md:pb-24">
