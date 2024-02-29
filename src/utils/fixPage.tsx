8d4af7a5fdea00b3b77e6bda9e1db67f7133b601
IamCooller <45273146+IamCooller@users.noreply.github.com>
2024-02-29 10:27:48 -0700

    Update fixPage.tsx

--- src/utils/fixPage.tsx
+++ src/utils/fixPage.tsx
@@ -3,17 +3,6 @@ export const FixPage = (
   elementRef: React.RefObject<HTMLElement>
 ) => {
   if (isFixed && elementRef.current) {
-    const windowHeight = window.innerHeight;
-    const elementHeight = elementRef.current.offsetHeight;
-    const elementTopPosition =
-      elementRef.current.getBoundingClientRect().top + window.scrollY;
-    const centeredPosition =
-      elementTopPosition + elementHeight / 2 - windowHeight / 2;
-
-    window.scrollTo({
-      top: centeredPosition,
-      behavior: 'auto'
-    });
     document.documentElement.style.overflow = 'hidden';
     document.body.style.paddingRight = '9px';
   } else {
