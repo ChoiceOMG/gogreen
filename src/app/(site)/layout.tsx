8b927dbae2fc55aa98af4ca5727c9bb4a16c145c
Peter Jaffray <peter@choice.marketing>
2024-03-25 21:27:17 +0000

    removed lottie

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,70 +1,69 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import { Avenir, openSans } from '@/styles/fonts/fonts';
 import '@/styles/globals.scss';
-import type { Metadata } from 'next';
 import { _siteUrl } from '@/utils/constants';
+import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import Loading from './loading';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import { Avenir, openSans } from '@/styles/fonts/fonts';
 
-import { GoogleTagManager } from '@next/third-parties/google';
 import { FixButton } from '@/components/UI/FixButton';
+import { GoogleTagManager } from '@next/third-parties/google';
 import Cursor from './Cursor';
-import { LogoAnimation } from '@/components/Animations/LogoAnimation';
+
 
 const title = 'GoGreen';
 const description =
-  'GoGreen is a cleaning company in Edmonton, Alberta. We provide commercial cleaning services for businesses in Edmonton and surrounding areas. We offer a wide range of cleaning services, including janitorial services, office cleaning, carpet cleaning, and more. We are committed to providing the highest quality cleaning services to our clients. Our team of professional cleaners is dedicated to delivering exceptional results. We are proud to be a leading cleaning company in Edmonton, Alberta. Contact us today to learn more about our cleaning services.';
+	'GoGreen is a cleaning company in Edmonton, Alberta. We provide commercial cleaning services for businesses in Edmonton and surrounding areas. We offer a wide range of cleaning services, including janitorial services, office cleaning, carpet cleaning, and more. We are committed to providing the highest quality cleaning services to our clients. Our team of professional cleaners is dedicated to delivering exceptional results. We are proud to be a leading cleaning company in Edmonton, Alberta. Contact us today to learn more about our cleaning services.';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(_siteUrl),
-  title: {
-    template: `%s | ${title}`,
-    default: title
-  },
-  description: description,
-  openGraph: {
-    title: title,
-    description: description,
-    url: _siteUrl,
-    siteName: title,
-    locale: 'en_CA',
-    type: 'website',
-    images: [{ url: `${_siteUrl}/images/logo.svg` }]
-  },
-  icons: {
-    shortcut: [{ url: `${_siteUrl}/images/logo.svg` }]
-  }
+	metadataBase: new URL(_siteUrl),
+	title: {
+		template: `%s | ${title}`,
+		default: title
+	},
+	description: description,
+	openGraph: {
+		title: title,
+		description: description,
+		url: _siteUrl,
+		siteName: title,
+		locale: 'en_CA',
+		type: 'website',
+		images: [{ url: `${_siteUrl}/images/logo.svg` }]
+	},
+	icons: {
+		shortcut: [{ url: `${_siteUrl}/images/logo.svg` }]
+	}
 };
 
 export default function RootLayout({
-  children
+	children
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html
-      lang="en"
-      className={` ${openSans.className} ${Avenir.variable} text-goGreen-black`}
-    >
-      <body>
-        <Cursor />
-        <Header />
-        <Suspense fallback={<Loading />}>
-          {' '}
-          <main className="relative min-h-screen">{children} </main>
-        </Suspense>
-        <FixButton />
-        <Footer />
-        <LogoAnimation />
-      </body>
-      {process.env.NODE_ENV === 'production' &&
-        process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
-          // Google Tag Manager
-          <GoogleTagManager
-            gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}
-          />
-        )}
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={` ${openSans.className} ${Avenir.variable} text-goGreen-black`}
+		>
+			<body>
+				<Cursor />
+				<Header />
+				<Suspense fallback={<Loading />}>
+					{' '}
+					<main className="relative min-h-screen">{children} </main>
+				</Suspense>
+				<FixButton />
+				<Footer />
+			</body>
+			{process.env.NODE_ENV === 'production' &&
+				process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
+					// Google Tag Manager
+					<GoogleTagManager
+						gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}
+					/>
+				)}
+		</html>
+	);
 }
