f37b6ee2838f18260ec3d20b98cd7f834b0ada3c
IamCooller <45273146+IamCooller@users.noreply.github.com>
2024-04-04 09:13:04 -0600

    Update helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,14 @@
 export function formatPhoneNumber(phoneNumber: string): string {
-  const cleaned = ('' + phoneNumber).replace(/\D/g, '');
+  let cleaned = phoneNumber.replace(/\D/g, '');
+
+  if (cleaned.startsWith('1')) {
+    cleaned = cleaned.substring(1);
+  }
+
   const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
   if (match) {
-    const formattedNumber = '1' + match[1] + match[2] + match[3];
-    return '+' + formattedNumber;
+    return '+1' + match[1] + match[2] + match[3];
   }
+
   return phoneNumber;
 }
